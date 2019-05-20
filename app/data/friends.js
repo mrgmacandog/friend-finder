b093232ce8b7f28649bef9d6832998eabdfd37bc
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-19 20:54:04 -0700

    Update survey.html to find match from friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,20 @@
 let friends = [
     {
-        "name":"Martin Macandog",
-        "photo":"https://media.licdn.com/dms/image/C5603AQFSxhoBZc01jw/profile-displayphoto-shrink_200_200/0?e=1564012800&v=beta&t=33QYmnFXuFU9oi-BeAcmLHW8wZcpoe7B82GgE_GYqD8",
-        "scores":[
-            1,
-            2,
-            3,
-            4,
-            5,
-            1,
-            2,
-            3,
-            4,
-            5
-          ]
-      }
+        "name": "Martin Macandog",
+        "photo": "https://media.licdn.com/dms/image/C5603AQFSxhoBZc01jw/profile-displayphoto-shrink_200_200/0?e=1564012800&v=beta&t=33QYmnFXuFU9oi-BeAcmLHW8wZcpoe7B82GgE_GYqD8",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    }
 ]
 
 // Export the friends array
