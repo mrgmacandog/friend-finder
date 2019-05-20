d5aff0999a4c422552b2419511b877d79c0f69cd
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-19 23:29:19 -0700

    Update survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ let friends = [
         "name": "Martin Macandog",
         "photo": "https://media.licdn.com/dms/image/C5603AQFSxhoBZc01jw/profile-displayphoto-shrink_200_200/0?e=1564012800&v=beta&t=33QYmnFXuFU9oi-BeAcmLHW8wZcpoe7B82GgE_GYqD8",
         "scores": [
-            "1",
-            "2",
-            "3",
             "4",
+            "4",
+            "3",
             "5",
-            "1",
+            "5",
             "2",
-            "3",
             "4",
-            "5"
+            "4",
+            "4",
+            "4"
         ]
     }
 ]
