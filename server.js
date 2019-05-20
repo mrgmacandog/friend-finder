7d3ce8abfc0c0ff2d901eb20dd394c49fa0af3d9
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-19 17:29:07 -0700

    Set up html routes with temp html pages

--- server.js
+++ server.js
@@ -4,11 +4,29 @@ const express = require("express");
 
 // Sets up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// TODO move html routes to htmlRoutes.js
+// ==============================================================
+// Set route for home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+// Set route for survey page
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+// Redirect all other routes to the home page
+app.get('*',function (req, res) {
+    res.redirect('/');
+});
+// ==============================================================
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
